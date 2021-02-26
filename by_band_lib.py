4f055e0c019ca5f7f9f5e5731da6118f77a3d118
rpernak <rpernak@aer.com>
2021-02-26 06:46:07 -0800

    re-added (ugly!) k-distribution recombination code; started forcing addition but hard-coding forcing index 0 for now

--- by_band_lib.py
+++ by_band_lib.py
@@ -611,7 +611,9 @@ class gCombine_Cost:
                 each band without any g-point reduction
             fluxesLBL -- string, path to LBLRTM flux netCDF file
             fluxesRRTMGP -- string, path to RRTMGP flux netCDF file
-            idxForce -- int, index of forcing scenario
+            idxForce -- dictionary with key for each cost function component
+                values are indices of associated forcing scenario for 
+                each component
             iCombine -- int, index for what iteration of g-point combining is
                 underway
 
@@ -640,7 +642,7 @@ class gCombine_Cost:
         self.distBands = dict(bandDict)
         self.lblNC = str(fluxesLBL)
         self.rrtmgpNC = str(fluxesRRTMGP)
-        self.iForce = int(idxForce)
+        self.iForce = dict(idxForce)
         self.iCombine = int(iCombine)
         self.profiles = str(profilesNC)
         self.exe = str(exeRRTMGP)
@@ -873,7 +875,7 @@ class gCombine_Cost:
                     # get array for variable, then compute its test-ref RMS
                     # over all columns at given pressure levels for a given
                     # forcing scenario
-                    cfDA = getattr(subsetErr, comp).isel(record=self.iForce)**2
+                    cfDA = getattr(subsetErr, comp).isel(record=0)**2
 
                     # components will be scaled by their own initial cost
                     self.costComps[comp].append(cfDA.sum(dim=['col']))
@@ -1074,8 +1076,197 @@ class gCombine_Cost:
         self.costComps = {}
     # end setupNextIter()
 
-    def calcOptFlux(self, kRefNC, exeFull=EXEFULL, ncFullProf=NCFULLPROF, 
-                    kOutNC='rrtmgp-data-lw-g-red.nc', 
+    def kDistOpt(self, kRefNC, kOutNC='rrtmgp-data-lw-g-red.nc'):
+        """
+        Combine the k-distributions from each of the bands after optimization
+        has been performed
+
+        TO DO: CLEAN THIS THE EFF UP!
+        """
+
+        ncFiles = [self.distBands[key].kInNC for key in self.distBands.keys()]
+
+        # wanna do an xarray.merge(), but it's not that simple
+        # initialize outputs
+        fullDict = {}
+
+        # what netCDF variables have a g-point dimension and will thus
+        # need to be modified in the combination iterations?
+        # TO DO: need SW
+        kMajor = ['kmajor', 'plank_fraction']
+
+        # kminor variables that are nontrivial to combine
+        # for minor contributors
+        regions = ['lower', 'upper']
+        kMinor = ['kminor_{}'.format(reg) for reg in regions]
+
+        strVars = ['identifier_minor', 'gas_minor', 'gas_names']
+        minorVars = []
+        for reg in regions:
+            minorVars.append('minor_gases_{}'.format(reg))
+            minorVars.append('scaling_gas_{}'.format(reg))
+        # end region loop
+
+        scalars = ['absorption_coefficient_ref_P',
+                   'absorption_coefficient_ref_T', 
+                   'press_ref_trop']
+
+        nGpt = []
+
+        # number of minor contributors per band
+        nPerBandUp = np.zeros(16).astype(int)
+        nPerBandLo = np.zeros(16).astype(int)
+        for iNC, ncFile in enumerate(ncFiles):
+            with xa.open_dataset(ncFile) as kDS:
+                sizeDS = kDS.sizes
+                nGpt.append(sizeDS['gpt'])
+                ncVars = list(kDS.keys())
+                for ncVar in ncVars:
+                    # don't know what to do with this! for the full band file, 
+                    # we probably don't need it anymore
+                    if ncVar == 'gpt_weights': continue
+
+                    varDA = kDS[ncVar]
+                    varArr = varDA.values
+                    varDims = varDA.dims
+                    varSizes = varDA.sizes
+
+                    # is the variable empty (e.g., no minor contributors)?
+                    if not varSizes:
+                        if ncVar in scalars: fullDict[ncVar] = varDA
+                        continue
+                    # endif empty var
+
+                    # minor contributors coming back to haunt me
+                    # no contributions in a given band
+                    minCond1 = ('minor_absorber_intervals_upper' in varDims or \
+                        'contributors_upper' in varDims) and \
+                        iNC in [3, 11, 13, 14, 15]
+                    minCond2 = ('minor_absorber_intervals_lower' in varDims or \
+                        'contributors_lower' in varDims) and iNC == 13
+                    if minCond1 or minCond2: continue
+
+                    vDim = 'minor_absorber_intervals_upper'
+                    if vDim in varDims:
+                        nPerBandUp[iNC] = varSizes[vDim]
+
+                    vDim = 'minor_absorber_intervals_lower'
+                    if vDim in varDims:
+                        nPerBandLo[iNC] = varSizes[vDim]
+
+                    concatAx = -1 if ncVar in kMajor + kMinor else 0
+                    if ncVar not in fullDict.keys():
+                        if ncVar in minorVars:
+                            fullDict[ncVar] = varArr.tolist()
+                        else:
+                            fullDict[ncVar] = varArr
+                        # endif ncVar
+                    elif '_ref' in ncVar or ncVar in strVars:
+                        # these are the same for every band and 
+                        # can be overwritten
+                        fullDict[ncVar] = varArr
+                    else:
+                        if ncVar in minorVars:
+                            fullDict[ncVar] += varArr.tolist()
+                        elif ncVar in strVars:
+                            fullDict[ncVar].append(varArr)
+                        else:
+                            fullDict[ncVar] = np.concatenate(
+                                (fullDict[ncVar], varArr), axis=concatAx)
+                        # endif ncVar
+                    # endif ncVar
+                # end ncVar loop
+            # endwith
+        # end band loop
+
+        # transform string lists
+        for sv in strVars: fullDict[sv] = np.array(fullDict[sv]).T
+        for mv in minorVars: fullDict[mv] = np.array(fullDict[mv]).T
+
+        # recalculate g-point related indices
+        fullDict['bnd_limits_gpt'] = \
+            (fullDict['bnd_limits_gpt']-[0,1]).flatten(
+            ).cumsum().reshape(-1, 2)
+
+        i1, iStartLo, iStartUp = 1, 1, 1
+        lims_gpt_up, lims_gpt_lo = [], []
+        startUp, startLo = [], []
+        for nG, nUp, nLo in zip(nGpt, nPerBandUp, nPerBandLo):
+            i2 = i1 + nG - 1
+            for i in range(nUp):
+                lims_gpt_up.append([i1, i2])
+                startUp.append(iStartUp)
+                iStartUp += nG
+            # end i loop
+
+            for i in range(nLo): 
+                lims_gpt_lo.append([i1, i2])
+                startLo.append(iStartLo)
+                iStartLo += nG
+            # end i loop
+
+            i1 += nG
+        # end minor contributor loop
+
+        fullDict['minor_limits_gpt_lower'] = np.array(lims_gpt_lo)
+        fullDict['minor_limits_gpt_upper'] = np.array(lims_gpt_up)
+        fullDict['kminor_start_lower'] = np.array(startLo)
+        fullDict['kminor_start_upper'] = np.array(startUp)
+
+        #for key in fullDict.keys(): print(key, fullDict[key].shape)
+        outDims = {
+            'bnd': len(ncFiles), 
+            'atmos_layer': 2, 
+            'pair': 2, 
+            'temperature': 14, 
+            'pressure_interp': 60, 
+            'mixing_fraction': 9, 
+            'gpt': sum(nGpt), 
+            'temperature_Planck': 196, 
+            'minor_absorber': 21, 
+            'absorber': 19, 
+            'minor_absorber_intervals_lower': len(lims_gpt_lo), 
+            'minor_absorber_intervals_upper': len(lims_gpt_up), 
+            'pressure': 59, 
+            'absorber_ext': 20, 
+            'fit_coeffs': 2, 
+            'contributors_lower': fullDict['kminor_lower'].shape[-1], 
+            'contributors_upper': fullDict['kminor_upper'].shape[-1],
+            'string_len': 32
+        }
+
+        # extract dimensions from reference LBL netCDF for consistency
+        outDict = {}
+        with xa.open_dataset(kRefNC) as refDS:
+            for key in fullDict.keys():
+                data = fullDict[key]
+                dims = refDS[key].dims
+                outDict[key] = {"dims": dims, "data": data}
+            # end key loop
+        # endwith
+
+        # dimensions after g-point reduction
+        """
+        outDims['gpt'] = sum(nGpt)
+        outDims['minor_absorber_intervals_lower'] = len(lims_gpt_lo)
+        outDims['minor_absorber_intervals_upper'] = len(lims_gpt_up)
+        outDims['contributors_lower'] = fullDict['kminor_lower'].shape[-1]
+        outDims['contributors_upper'] = fullDict['kminor_upper'].shape[-1]
+        """
+
+        # coordinates for xarray dataset
+        outCoord = {}
+        for key in outDims.keys(): 
+            outCoord[key] = {'dims': (key), "data": range(outDims[key])}
+
+        # make an acceptable dictionary for xarray.Dataset.from_dict()
+        dsDict = {"coords": outCoord, "dims": outDims, "data_vars": outDict}
+
+        outDS = xa.Dataset.from_dict(dsDict)
+        outDS.to_netcdf(kOutNC)
+    # end kDistOpt()
+    
+    def calcOptFlux(self, exeFull=EXEFULL, ncFullProf=NCFULLPROF, 
                     fluxOutNC='optimized_fluxes.nc'):
         """
         Once the optimized solution has been found, combine k-distribution
@@ -1089,7 +1280,5 @@ class gCombine_Cost:
 
         finalDS = combineBands(0, fullDS, fullDS[0], finalDS=True)
         finalDS.to_netcdf(fluxOutNC)
-
-        return
     # end calcOptFlux()
 # end gCombine_Cost
