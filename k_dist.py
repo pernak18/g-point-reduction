8c6c02dadcc1bccea3aa095160a0607313c4bfa1
rpernak <rpernak@aer.com>
2021-03-16 10:30:45 -0700

    cleanup of unnecessary variables

--- k_dist.py
+++ k_dist.py
@@ -7,8 +7,6 @@ os.chdir('/global/u1/p/pernak18/RRTMGP/g-point-reduction')
 # "standard" install
 import numpy as np
 
-from multiprocessing import Pool
-
 # directory in which libraries installed with conda are saved
 PIPPATH = '{}/.local/'.format(os.path.expanduser('~')) + \
     'cori/3.7-anaconda-2019.10/lib/python3.7/site-packages'
@@ -26,50 +24,24 @@ import xarray as xa
 # local module
 import by_band_lib as BYBAND
 
-PROJECT = '/global/project/projectdirs/e3sm/pernak18/'
-EXE = '{}/g-point-reduction/garand_atmos/rrtmgp_garand_atmos'.format(
-    PROJECT)
-REFDIR = '{}/reference_netCDF/g-point-reduce'.format(PROJECT)
-
-KFULLNC = '{}/rrtmgp-data-lw-g256-2018-12-04.nc'.format(REFDIR)
-GARAND = '{}/multi_garand_template_single_band.nc'.format(REFDIR)
-
-# test (RRTMGP) and reference (LBL) flux netCDF files
-TESTNC = '{}/rrtmgp-lw-flux-inputs-outputs-garandANDpreind.nc'.format(REFDIR)
-REFNC = '{}/lblrtm-lw-flux-inputs-outputs-garandANDpreind.nc'.format(REFDIR)
-PATHS = [KFULLNC, EXE, TESTNC, REFNC, GARAND]
+GARAND = '/global/project/projectdirs/e3sm/pernak18/reference_netCDF/' + \
+    'g-point-reduce/multi_garand_template_single_band.nc'
+BYBAND.pathCheck(GARAND)
 
 BANDSPLITDIR = 'band_k_dist'
 FULLBANDFLUXDIR = 'full_band_flux'
 
-for PATH in PATHS: BYBAND.pathCheck(PATH)
-
 CWD = os.getcwd()
 
 # only do one domain or the other
-DOLW = True
+DOLW = False
 DOSW = not DOLW
 DOMAIN = 'LW' if DOLW else 'SW'
 NBANDS = 16 if DOLW else 14
 
-# forcing scenario (0 is no forcing...need a more comprehensive list)
-IFORCING = 0
-
-# does band-splitting need to be done, or are there existing files 
-# that have divided up the full k-distribution?
-BANDSPLIT = False
-
 # remove the netCDFs that are generated for all of the combinations 
 # and iterations of combinations in bandOptimize()
 CLEANUP = False
-
-# number of iterations for the optimization
-NITER = 1
-
-# cost function variables
-CFCOMPS = ['band_flux_dn', 'band_flux_up']
-CFLEVS = [0, 10000, 102000] # pressure levels of interest in Pa
-CFWGT = [0.5, 0.5]
 
 kFiles = sorted(glob.glob('{}/coefficients_{}_band??.nc'.format(
         BANDSPLITDIR, DOMAIN)))
